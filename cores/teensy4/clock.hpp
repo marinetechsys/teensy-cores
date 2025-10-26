7d2241df23d29a6eae9f19bca9258b72c31bddd2
Adam Phoenix <aphoenix@blueoceanmts.com>
2025-10-26 11:05:02 +0800

    Add header, fix tick

---
+++ cores/teensy4/clock.hpp
@@ -0,0 +1,9 @@
+#ifndef TEENSY_CORES_CLOCK_HPP
+#define TEENSY_CORES_CLOCK_HPP
+
+#ifdef __cplusplus
+extern uint64_t get_us();
+extern uint64_t get_us_from_isr();
+#endif
+
+#endif // TEENSY_CORES_CLOCK_HPP
--- cores/teensy4/startup.c
+++ cores/teensy4/startup.c
@@ -249,7 +249,10 @@ FLASHMEM static void configure_systick(void)
 	SYST_RVR = (SYSTICK_EXT_FREQ / 1000) - 1;
 	SYST_CVR = 0;
 	SYST_CSR = SYST_CSR_TICKINT | SYST_CSR_ENABLE;
-	SCB_SHPR3 = 0x20200000;  // Systick, pendablesrvreq_isr = priority 32;
+
+	uint8_t priority = 2 << (8 - __NVIC_PRIO_BITS); // shift if needed
+    SCB_SHPR3 = (priority << 24) | (priority << 16);
+
 	ARM_DEMCR |= ARM_DEMCR_TRCENA;
 	ARM_DWT_CTRL |= ARM_DWT_CTRL_CYCCNTENA; // turn on cycle counter
 	systick_cycle_count = ARM_DWT_CYCCNT; // compiled 0, corrected w/1st systick
